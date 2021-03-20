b80845c4d142185b31400dc62b64807922e488d3
gUREOLi <gustavo.oliveira145@fatec.sp.gov.br>
2021-03-20 16:07:59 -0300

    gustavo

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-
+let c 
 
 $(document).ready(()=>{
+    if(localStorage.getItem("contagem") != null){
+        c = localStorage.getItem("contagem")
+    }
+    else{
+        c = 0
+    }
+    document.getElementById("contador-carrinho").innerHTML = c
     $(window).on("load", ()=>{
         $(".load-page").hide()
     })
 })
 
-
+function contar(){
+    c++
+    localStorage.setItem("contagem", c)
+    document.getElementById("contador-carrinho").innerHTML = c
+}
